0f793564d4bc12655a38cfb4152947adec7950ee
s0oramy <soo031100@naver.com>
2021-07-14 16:37:15 +0900

    크로아티아 알파벳

---
+++ Step07_문자열/Step07_09.py
@@ -0,0 +1,18 @@
+croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+word = input()
+
+for i in croatia :
+    word = word.replace(i, '*')  
+print(len(word))
+
+'''
+word = input()
+count = 0
+for w in range(len(word)) : 
+    if word[w] == '=' or word[w] == '-': count += 0
+    elif word[w] == 'z' or word[w] == 'j' :
+        if word[w-1] == 'd' or word[w-1] == 'n' or word[w-1] == 'l': count += 0
+        else: count += 1
+    else : count += 1
+print(count)
+'''
