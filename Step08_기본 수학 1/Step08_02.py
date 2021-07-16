61baead9d37620a5f0feb51cdf7bbab938faebf2
s0oramy <soo031100@naver.com>
2021-07-16 11:44:25 +0900

    벌집

--- Step08_기본 수학 1/Step08_02.py
+++ Step08_기본 수학 1/Step08_02.py
@@ -1,8 +1,6 @@
 n = int(input())
-
-nums_pileup = 1  
-count = 1
-while n > nums_pileup :
-    nums_pileup += 6 * count  
-    count += 1  
-print(count)
+cnt = 1
+while n > 1:
+    n -= (6 * cnt)
+    cnt += 1
+print(cnt)
